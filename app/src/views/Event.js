c0763648f8b29fae4af2603ef9836f784a2cef38
Jani Eväkallio <jani.evakallio@gmail.com>
2018-08-27 16:45:00 +0100

    Wire up Event view with navigation params

--- app/src/views/Event.js
+++ app/src/views/Event.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import styled from 'styled-components/native';
 
-import Container from '../components/Container';
-
-const Title = styled.Text`
-  font-size: 20px;
+const Wrapper = styled.View`
+  padding: 20px;
 `;
 
-const Home = () => (
-  <Container>
-    <Title>Hi</Title>
-  </Container>
-);
+const Title = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  margin-bottom: 4px;
+`;
+
+const Speaker = styled.Text`
+  font-size: 12px;
+  font-style: italic;
+`;
+
+const Agenda = styled.Text`
+  font-size: 12px;
+  line-height: 14px;
+  margin-top: 14px;
+`;
+
+const Home = ({ navigation }) => {
+  const event = navigation.getParam('event', {});
+  return (
+    <Wrapper>
+      <Title>{event.title}</Title>
+      <Speaker>{event.speaker}</Speaker>
+      <Agenda>{event.agenda}</Agenda>
+    </Wrapper>
+  );
+};
 
 export default Home;
